4ab1676a73904c2ef1ceefb299df266802d0cee2
scottz0r <scott.clewell@gmail.com>
2023-12-12 18:54:50 -0600

    Linux filemode

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"go-zero-apr-mgr/mvc"
 	za "go-zero-apr-mgr/zero-apr-lib"
+	"os"
 )
 
 func main() {
-	da, err := za.Connect("test.json")
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid arguments. Expected json file path.")
+		return
+	}
+
+	dataFile := os.Args[1]
+
+	da, err := za.Connect(dataFile)
 	if err != nil {
 		fmt.Println(err)
 		return
